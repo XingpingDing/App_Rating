f8823a8fbe0a338a552697e138e641e12935552c
Xingping Ding <xingpingding@gmail.com>
2016-03-23 00:58:11 +0000

    delete

--- AppRating/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from AppRating.models import Category, App, Comment
-
-admin.site.register(Category)
-admin.site.register(App)
-admin.site.register(Comment)
--- AppRating/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppratingConfig(AppConfig):
-    name = 'AppRating'
